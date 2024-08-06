cc7ea929bf0bca7a7fe54170e9953b1942efe003
lalahsantana <160791605+lalahsantana@users.noreply.github.com>
2024-08-06 20:16:27 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,2 @@
+# VS2_git
+# main.pyfrom pessoa import Pessoanome = str(input('Digite o nome: '))idade = int(input('Digite a idade: '))pessoa1 = Pessoa(nome, idade)print(f'Seu ano de nascimento é: {pessoa1.calcular_ano()}')
